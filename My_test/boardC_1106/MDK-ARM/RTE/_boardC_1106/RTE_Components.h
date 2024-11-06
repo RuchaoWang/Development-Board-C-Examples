3ad2cd4a09fe94c044e9cc48cfcf5b47a4e35a03
RUCHAO <2110099@stu.neu.edu.cn>
2024-11-06 15:09:23 +0800

    C板点灯

---
+++ My_test/boardC_1106/MDK-ARM/RTE/_boardC_1106/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'boardC_1106' 
+ * Target:  'boardC_1106' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
